fc15a7c8eab5d823f50a7f581812756a16a358ec
Parag0712 <paragvadgama123@gmail.com>
2024-03-08 17:24:39 +0530

    title added

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap"
           rel="stylesheet"
         />
+        <title>Your Page Title</title>
       </Head>
       <body className="font-poppins">
+        
         <Main />
         <NextScript />
       </body>
